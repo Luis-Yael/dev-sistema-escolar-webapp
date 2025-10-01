ac23c904d787fdb24321f52a127d1e78fe1d20a3
Luis Yael Mendez Sanchez <luisyaelms@gmail.com>
2025-10-01 10:18:50 -0600

    Form del admin
    Se diseña y se valida el formulario del administrador desde el frontend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,22 @@ import { LoginScreenComponent } from './screens/login-screen/login-screen.compon
 import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { RegistroAdminComponent } from './partials/registro-admin/registro-admin.component';
+import { RegistroAlumnosComponent } from './partials/registro-alumnos/registro-alumnos.component';
+import { RegistroMaestrosComponent } from './partials/registro-maestros/registro-maestros.component';
 
 //Angular material
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatRadioModule} from '@angular/material/radio';
-import { RegistroAdminComponent } from './partials/registro-admin/registro-admin.component';
-import { RegistroAlumnosComponent } from './partials/registro-alumnos/registro-alumnos.component';
-import { RegistroMaestrosComponent } from './partials/registro-maestros/registro-maestros.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 //Ngx-cookie-service
 import { CookieService } from 'ngx-cookie-service';
+
+// Third Party Modules
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -42,10 +47,14 @@ import { CookieService } from 'ngx-cookie-service';
     HttpClientModule,
     MatCardModule,
     MatIconModule,
-    MatRadioModule
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgxMaskDirective
   ],
   providers: [
-    CookieService
+    CookieService,
+    provideNgxMask()
   ],
   bootstrap: [AppComponent]
 })
